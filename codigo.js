b307e482bc0dee32089012ee575550d9b16938cb
pdrocrr <ph736230@gmail.com>
2026-03-23 21:20:14 -0300

    meu primeiro commit

---
+++ codigo.js
@@ -0,0 +1,13 @@
+let pokemons = [
+["Poochyena", "Lvl 2","M"],
+["Zigzagoon","Lvl 2", "F"],
+["Dragonite", "Lvl 5", "M"],
+["Dragonite", "Lvl 5", "F"],
+["Dragonite", "Lvl 5", "F"],
+["Poochyena", "Lvl 3", "F"],
+["Wurmple", "Lvl 2", "M"] 
+]
+console.log("=====Pokemons cadastrados=====");
+ for(let i = 0; i < pokemons.length; i++) {
+     console.log(pokemons[i][0]) 
+    } 
